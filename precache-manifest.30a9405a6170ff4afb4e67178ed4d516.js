6451ae5c75a33b09cb06d6269b5f2cfeb9da1635
Dave Thompson <dave@brightec.co.uk>
2020-06-10 13:45:00 +0100

    Updates

---
+++ precache-manifest.30a9405a6170ff4afb4e67178ed4d516.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdf027f499c3c93e920d39ce8ca415bd",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0139737ddbbb9595216a",
+    "url": "/monsters-rolodex/static/css/main.1cf49836.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0139737ddbbb9595216a",
+    "url": "/monsters-rolodex/static/js/main.06d04b61.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
